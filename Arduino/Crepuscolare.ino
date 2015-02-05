fb9c281cf08ea9c1abac2b2cbd469ad66e1312d7
Loreton <loreto.n@gmail.com>
2015-02-05 11:41:19 +0100

    2015-02-05 10:41 - commit with no comment

---
+++ Arduino/Crepuscolare.ino
@@ -0,0 +1,176 @@
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+ This example code is in the public domain.
+ */
+
+
+int THR_Pin         = A0;   // select the input pin for the potentiometer
+int THR_Value       = 0;    // valore analogico del potenziometro
+
+int LDR_Pin         = A3;   // select the input pin for the LDR
+int LDR_Value       = 0;    // valore analogico LDR
+
+int LED             = 5;   // select the pin for the LED (13 e' quello incorporato)
+int Relay           = 3;
+
+int debounce       = 0;
+#define LOOPTIME   100  // mSecondi
+// #define DEBOUCE     3*(1000/LOOPTIME)
+#define DEBOUNCE_MAX     15
+
+// the setup routine runs once when you press reset:
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(Relay, OUTPUT);
+    digitalWrite(LED, LOW);
+    digitalWrite(Relay, LOW);
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    // checkOUTPUT();
+    // delay(250); // in mSec
+
+        // ------------------------------------
+        // - leggi i due pin analogici
+        // - LDR diminuisce all'aumentare della luce
+        // - THR diminuisce girando il trimmer in CW
+        // ------------------------------------
+    LDR_Value = readAnalogPin(LDR_Pin);
+    THR_Value = readAnalogPin(THR_Pin);
+    // LDR_Value = 1023-LDR_Value;             //lo invertiamo per non invertire i fili del potenziometro.
+    THR_Value = 1023-THR_Value;             //lo invertiamo per non invertire i fili del potenziometro.
+
+
+    float LDR_Voltage = LDR_Value * (5.0 / 1023.0);
+    float THR_Voltage = THR_Value * (5.0 / 1023.0);
+
+        // *** SOLO per DEBUG
+    Serial.print("LDR - Value :"); Serial.print(LDR_Value, DEC); Serial.print("  LDR - Volt     :"); Serial.println(LDR_Voltage, 2);
+    Serial.print("THR - Value :"); Serial.print(THR_Value, DEC); Serial.print("  THR - Volt     :"); Serial.println(THR_Voltage, 2);
+
+        // print out the value you read:
+    if (LDR_Value > THR_Value) {
+        if (debounce < DEBOUNCE_MAX)
+            debounce += 1;
+
+        // Serial.print("  LED: "); Serial.print("ON ");Serial.print("  (debounce:");Serial.println(debounce);
+    }
+    else {
+        if (digitalRead(Relay) == LOW) debounce = 0;
+        if (debounce > 0) debounce -= 1;
+        // Serial.print("  LED: "); Serial.print("OFF ");Serial.print("  (debounce:");Serial.println(debounce);
+    }
+
+    // Serial.print("  LED: "); Serial.print(digitalRead(LED));Serial.print("  (debounce:");Serial.println(debounce);
+    Serial.print("  debounce:");Serial.println(debounce);
+    Serial.println();
+
+
+    checkOUTPUT();
+    delay(250); // in mSec
+    checkOUTPUT();
+
+/*
+    if (debounce == DEBOUNCE_MAX) {
+        if (digitalRead(LED)   == LOW)   digitalWrite(LED, HIGH);
+        if (digitalRead(Relay) == LOW) digitalWrite(Relay, HIGH);
+    }
+    else if (debounce > 0) {
+        if (digitalRead(LED) == HIGH) // read the input pin
+            digitalWrite(LED, LOW);
+        else
+            digitalWrite(LED, HIGH);
+    }
+    else {
+        debounce = 0;
+        if (digitalRead(LED)   == HIGH)   digitalWrite(LED, LOW);
+        if (digitalRead(Relay) == HIGH) digitalWrite(Relay, LOW);
+    }
+
+*/
+
+
+}
+
+
+
+
+
+// ##################################################
+// #
+// ##################################################
+void checkOUTPUT() {
+
+    // LEDSTatus = digitalRead(LED);
+    // LEDSTatus = digitalRead(LED);
+
+    if (debounce == DEBOUNCE_MAX) {
+        if (digitalRead(LED)   == LOW) digitalWrite(LED, HIGH);
+        if (digitalRead(Relay) == LOW) digitalWrite(Relay, HIGH);
+    }
+    else if (debounce > 0) {        // facciamo lampeggiare il LED
+        if (digitalRead(LED) == HIGH) // read the input pin
+            digitalWrite(LED, LOW);
+        else
+            digitalWrite(LED, HIGH);
+    }
+    else {                          // Spegniamo tutto
+        debounce = 0;
+        if (digitalRead(LED)   == HIGH)   digitalWrite(LED, LOW);
+        if (digitalRead(Relay) == HIGH) digitalWrite(Relay, LOW);
+    }
+
+    // LEDSTatus = digitalRead(LED);
+    // Serial.print("  LED: "); Serial.print(digitalRead(LED));Serial.print("  (debounce:");Serial.println(debounce);
+    // Serial.println();
+
+}
+
+// ##################################################
+// #
+// ##################################################
+int readAnalogPin(int pin) {
+const int MAX_READS = 10;
+int readings[MAX_READS];           // the readings from the analog input
+int readInx = 0;              // the index of the current reading
+int totValue = 0;                  // the running totValue
+int avgValue = 0;                // the avgValue
+int MIN = 2000;
+int MAX= 0;
+
+        // Lettura del pin
+    for (readInx = 0; readInx < MAX_READS; readInx++) {
+        readings[readInx] = analogRead(pin);    // lettura pin
+
+        // add the reading to the totValue:
+        totValue = totValue + readings[readInx];
+        delay(100);        // delay in between reads for stability
+    }
+
+    // calculate the avgValue:
+    avgValue = totValue / MAX_READS;
+
+        // DEBUG Display valori
+    boolean fDEBUG = false;
+    if (fDEBUG) {
+        Serial.print("[PIN ");Serial.print(pin, HEX);Serial.print("] - ");
+        for (readInx = 0; readInx < MAX_READS; readInx++) {
+            Serial.print(" ");Serial.print(readings[readInx], DEC);
+        }
+        Serial.print(" avgValue = ");Serial.print(avgValue, DEC);
+        float Voltage = avgValue * (5.0 / 1023.0);
+        Serial.print(" Voltage = ");Serial.println(Voltage, 2);
+        Serial.println();
+    }
+    #if 0
+    #endif
+
+
+    return avgValue;
+}
---
+++ KiCad/Crepuscolare.sch
@@ -0,0 +1,87 @@
+EESchema Schematic File Version 2
+LIBS:Ln_R-C-L
+LIBS:LnConnectors
+LIBS:LnDevice
+LIBS:LnDiodiTransistors
+LIBS:LnArduino_uno
+LIBS:LnMicroController
+LIBS:LnRegulators
+LIBS:LnSample
+LIBS:power
+LIBS:device
+LIBS:transistors
+LIBS:conn
+LIBS:linear
+LIBS:regul
+LIBS:74xx
+LIBS:cmos4000
+LIBS:adc-dac
+LIBS:memory
+LIBS:xilinx
+LIBS:special
+LIBS:microcontrollers
+LIBS:dsp
+LIBS:microchip
+LIBS:analog_switches
+LIBS:motorola
+LIBS:texas
+LIBS:intel
+LIBS:audio
+LIBS:interface
+LIBS:digital-audio
+LIBS:philips
+LIBS:display
+LIBS:cypress
+LIBS:siliconi
+LIBS:opto
+LIBS:atmel
+LIBS:contrib
+LIBS:valves
+EELAYER 27 0
+EELAYER END
+$Descr A4 11693 8268
+encoding utf-8
+Sheet 1 1
+Title ""
+Date "2 feb 2015"
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Comp
+L ARDUINO_NANO_IC U?
+U 1 1 54CF3065
+P 3600 3050
+F 0 "U?" H 4100 3200 60  0000 C CNN
+F 1 "ARDUINO_NANO_IC" V 4200 1900 60  0000 C CNN
+F 2 "" H 4300 800 60  0000 C CNN
+F 3 "" H 4300 800 60  0000 C CNN
+	1    3600 3050
+	1    0    0    -1  
+$EndComp
+$Comp
+L POT RV?
+U 1 1 54CF3092
+P 2150 4550
+F 0 "RV?" H 2150 4450 50  0000 C CNN
+F 1 "POT" H 2150 4550 50  0000 C CNN
+F 2 "~" H 2150 4550 60  0000 C CNN
+F 3 "~" H 2150 4550 60  0000 C CNN
+	1    2150 4550
+	0    1    1    0   
+$EndComp
+$Comp
+L RESISTOR R?
+U 1 1 54CF30A1
+P 1750 4050
+F 0 "R?" H 1825 4150 60  0000 C CNN
+F 1 "RESISTOR" H 2000 4050 60  0000 C CNN
+F 2 "~" H 1750 4050 60  0000 C CNN
+F 3 "~" H 1750 4050 60  0000 C CNN
+	1    1750 4050
+	1    0    0    -1  
+$EndComp
+$EndSCHEMATC
